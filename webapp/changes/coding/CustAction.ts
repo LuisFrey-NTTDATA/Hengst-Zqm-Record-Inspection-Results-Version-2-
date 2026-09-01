5a5a06b544a0df73b795113bf427e211da9ffe75
Luis Frey <luis.frey@nttdata.com>
2026-09-01 14:54:27 +0000

    last changes

--- webapp/changes/coding/CustAction.ts
+++ webapp/changes/coding/CustAction.ts
@@ -1,18 +1,40 @@
 import ControllerExtension from "sap/ui/core/mvc/ControllerExtension";
+import Fragment from "sap/ui/core/Fragment";
+import Dialog from "sap/m/Dialog";
 
 /**
  * @namespace customer.zqm.recinspresult
  * @controller
  */
 export default class CustAction extends ControllerExtension {
+  private _oDocumentDialog?: Dialog;
+
   overrides = {
     /**
      * Called when a controller is instantiated and its View controls (if available) are already created.
-     * Can be used to modify the View before it is displayed, to bind event handlers and do other one-time initialization.
-     * @memberOf customer.zqm.recinspresult.CustAction
      */
     onInit(this: CustAction) {
       const view = this.getView();
     },
   };
+
+  public async onOpenDocumentDialog(this: CustAction): Promise<void> {
+    const oView = this.getView();
+
+    if (!this._oDocumentDialog) {
+      this._oDocumentDialog = (await Fragment.load({
+        id: oView.getId(),
+        name: "customer.zqm.recinspresult.changes.fragments.DocumentDialog",
+        controller: this
+      })) as Dialog;
+
+      oView.addDependent(this._oDocumentDialog);
+    }
+
+    this._oDocumentDialog.open();
+  }
+
+  public onCloseDocumentDialog(this: CustAction): void {
+    this._oDocumentDialog?.close();
+  }
 }
